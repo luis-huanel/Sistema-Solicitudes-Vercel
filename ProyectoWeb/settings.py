f1ff48349ec246f2168a060831ff14443b5a95ed
BychuDev <151086019+luis-huanel@users.noreply.github.com>
2025-04-20 03:05:12 -0300

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'django_user_agents.middleware.UserAgentMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'ProyectoWeb.urls'
